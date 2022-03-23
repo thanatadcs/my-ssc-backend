957b2c27233a105b639d58fa24b177c7ad297868
thanatadcs <thanatad.cs@gmail.com>
2022-03-23 15:21:46 +0700

    Change backend port to 8081

--- src/main/java/io/muzoo/ssc/project/backend/auth/AuthenticationController.java
+++ src/main/java/io/muzoo/ssc/project/backend/auth/AuthenticationController.java
@@ -2,8 +2,6 @@ package io.muzoo.ssc.project.backend.auth;
 
 import io.muzoo.ssc.project.backend.SimpleResponseDTO;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
